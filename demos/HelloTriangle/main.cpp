21564d92392c8fbe08861178fefafe6f9892647c
virgilwjj <virgilwjj@gmail.com>
2022-04-25 15:36:03 +0800

    mod HelloTriangle

--- demos/HelloTriangle/main.cpp
+++ demos/HelloTriangle/main.cpp
@@ -112,7 +112,12 @@ int main() {
   }
 
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f,
+      0.5f,  0.5f,  0.0f, 0.5f,  -0.5f, 0.0f,
+      -0.5f, -0.5f, 0.0f, -0.5f, 0.5f,  0.0f,
+  };
+
+  unsigned int indices[] = {
+      0, 1, 3, 1, 2, 3,
   };
 
   GLuint VAO;
@@ -123,9 +128,16 @@ int main() {
   glGenBuffers(1, &VBO);
   SCOPE_EXIT { glDeleteBuffers(1, &VBO); };
 
+  GLuint EBO;
+  glGenBuffers(1, &EBO);
+  SCOPE_EXIT { glDeleteBuffers(1, &EBO); };
+
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -137,7 +149,7 @@ int main() {
 
     glUseProgram(shader_program);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
